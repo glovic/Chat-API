28ebe5fc70b8a72e8cb3f482a61622c8854e9b95
glovic <xtineviktor@gmail.com>
2024-10-18 19:11:01 +0100

    feat: Add Group model to store group data and messages

--- models/Group.js
+++ models/Group.js
@@ -1,16 +1,13 @@
-// models/Group.js
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    messages: [
-        {
-            sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-            content: { type: String, required: true },
-            timestamp: { type: Date, default: Date.now }
-        }
-    ]
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of user IDs
+    messages: [{
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // ID of the sender
+        message: String,
+        timestamp: { type: Date, default: Date.now } // Message timestamp
+    }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Group', groupSchema);
